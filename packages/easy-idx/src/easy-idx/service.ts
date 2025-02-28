6084f8f16ee0a2bde2e5cbf832612a22e2b6c0db
assafBarash <assaf@safebooks.ai>
2025-03-01 01:12:41 +0200

    chore(easy-idx): cleanup

--- packages/easy-idx/src/easy-idx/service.ts
+++ packages/easy-idx/src/easy-idx/service.ts
@@ -2,7 +2,7 @@ import fg from 'fast-glob';
 import path from 'path';
 import { Project, QuoteKind } from 'ts-morph';
 import { readFilesExports } from './read-files-exports';
-import { IndexItConfiguration } from './types';
+import { DirHandlerConfig, IndexItConfiguration } from './types';
 import { createIndexFile } from './create-index-file';
 
 export const IndexIt = async ({ paths, ...config }: IndexItConfiguration) => {
@@ -13,10 +13,6 @@ export const IndexIt = async ({ paths, ...config }: IndexItConfiguration) => {
   morph.manipulationSettings.set({ quoteKind: QuoteKind.Single });
 
   await Promise.all(paths.map(createDirHandler({ morph, ...config })));
-};
-
-type DirHandlerConfig = Omit<IndexItConfiguration, 'paths'> & {
-  morph: Project;
 };
 
 const createDirHandler =
