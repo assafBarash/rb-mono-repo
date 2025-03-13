5f7ec95011685e622f433dd80d97b4f7acf7ec85
assafBarash <assaf@safebooks.ai>
2025-03-13 18:24:59 +0200

    wip

--- packages/playground/src/main.ts
+++ packages/playground/src/main.ts
@@ -1,15 +1,26 @@
-import { Type, type Static } from '@sinclair/typebox';
-import { Value } from '@sinclair/typebox/value';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export function pipe<
+  T,
+  Fns extends [(arg: T) => any, ...Array<(arg: any) => any>]
+>(value: T, ...fns: Fns): PipeResult<T, Fns> {
+  return fns.reduce((acc, fn) => fn(acc), value) as PipeResult<T, Fns>;
+}
 
-const UserSchema = Type.Object({
-  name: Type.String(),
-  age: Type.Number()
-});
+// 🔥 Recursively infer the final return type
+type PipeResult<T, Fns extends Array<(arg: any) => any>> = Fns extends [
+  (arg: infer A) => infer B,
+  ...infer Rest
+]
+  ? Rest extends Array<(arg: any) => any>
+    ? PipeResult<B, Rest>
+    : B
+  : T;
 
-type User = Static<typeof UserSchema>;
-
-export const main = async (): Promise<User> => {
-  Value.Assert(UserSchema, { name: 'John', age: 42 });
-
+export const main = async () => {
+  pipe(
+    1,
+    (x) => x.toString(),
+    (v) => v // now v is any though it should be string;
+  );
   return { name: 'John', age: 42 };
 };
